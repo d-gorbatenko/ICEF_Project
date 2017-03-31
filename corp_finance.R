bd0ad902849e3064080061d987c91745e347f925
Dmitry Gorbatenko <gorbatenko@gettaxi.com>
2017-03-31 15:28:47 +0300

    kind of finish

--- corp_finance.R
+++ corp_finance.R
@@ -21,22 +21,35 @@ for (i in c(paste0("data_",c(1:((ncol(data) - 1) / 2) - 1)))) {
 }
 
 # run OLS regressions
-model <- lm(data = data_0, reten_on_index ~ 1 + reten_on_index_market)
 
+# model <- lm(data = data_0, reten_on_index ~ 1 + reten_on_index_market)
+# 
+# 
+# sigma_market <- var(data_0$reten_on_index_market, na.rm = T)
+# sigma_residual <- var(model$residuals, na.rm = T)
+# sq_diff_return <- (data_0$reten_on_index_market - mean(data_0$reten_on_index_market, na.rm = T)) ** 2
+# L_1 <- nrow(data_0)
+# sigma_ar <- sigma_residual + (1 / L_1) * (1 + sq_diff_return / sigma_market)
+# 
+# model$residuals / sigma_ar[c(2:25)]
+# 
+# 
+# data_0[c((13-window):(13+window)), ]
+# 
+# colnames(data_0)[2]
+# 
+# temp <- get("data_0")
+# temp <- temp[c((13-window):(13+window)), ]
+# result[j, 1] <- colnames(temp)[2]
 
-sigma_market <- var(data_0$reten_on_index_market, na.rm = T)
-sigma_residual <- var(model$residuals, na.rm = T)
-sq_diff_return <- (data_0$reten_on_index_market - mean(data_0$reten_on_index_market, na.rm = T)) ** 2
-L_1 <- nrow(data_0)
-sigma_ar <- sigma_residual + (1 / L_1) * (1 + sq_diff_return / sigma_market)
-
+result <- data.frame(name = rep_len(0, ((ncol(data) - 1) / 2)),
+                     name = rep_len(0, ((ncol(data) - 1) / 2)))
 window <- 5
-data_0[c((13-window):(13+window)), ]
-
+j <- 1
 for (i in c(paste0("data_",c(1:((ncol(data) - 1) / 2) - 1)))) {
         temp <- get(i)
         temp <- temp[c((13-window):(13+window)), ]
-        model <- ls(data = temp, reten_on_index ~ 1 + reten_on_index_market)
+        model <- lm(data = temp, reten_on_index ~ 1 + reten_on_index_market)
         
         sigma_market <- var(temp$reten_on_index_market, na.rm = T)
         sigma_residual <- var(model$residuals, na.rm = T)
@@ -44,6 +57,17 @@ for (i in c(paste0("data_",c(1:((ncol(data) - 1) / 2) - 1)))) {
         L_1 <- nrow(temp)
         sigma_ar <- sigma_residual + (1 / L_1) * (1 + sq_diff_return / sigma_market)
         
+        temp[, 6] <- model$residuals / sigma_ar
+        names(temp)[6] <- "strange_shit"
+        
+        # result <- data.frame(name = rep_len(0, ((ncol(data) - 1) / 2)),
+        #                      name = rep_len(0, ((ncol(data) - 1) / 2)))
+        
+        result[j, 1] <- colnames(temp)[2]
+        result[j, 2] <- mean(temp$strange_shit)
+        
+        j <- j + 1
+        colnames(temp)
         
         assign(i, temp)
 }
